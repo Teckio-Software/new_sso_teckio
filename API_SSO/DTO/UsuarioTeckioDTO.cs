1cc2b6ed59543b17450f74811d7d679884d0c873
ByJavvi <agama@procomi.com>
2026-01-19 11:58:51 -0600

    🚧 Añadido de endponit para inicio de sesión

---
+++ API_SSO/DTO/UsuarioTeckioDTO.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API_SSO.DTO
+{
+    public class RespuestaAutenticacionDTO
+    {
+        /// <summary>
+        /// Token temporal para ingresar al software
+        /// </summary>
+        public string Token { get; set; }
+        /// <summary>
+        /// Fecha de expiración del token para entrar al sistema
+        /// </summary>
+        public DateTime FechaExpiracion { get; set; }
+    }
+    /// <summary>
+    /// Para entrar en sistema y para la creación de un nuevo Usuario
+    /// </summary>
+    public class CredencialesUsuarioDTO
+    {
+        /// <summary>
+        /// Correo electrónico del usuario
+        /// </summary>
+        [Required]
+        public string Email { get; set; }
+        /// <summary>
+        /// Contraseña del usuario
+        /// </summary>
+        [Required]
+        public string Password { get; set; }
+    }
+}
